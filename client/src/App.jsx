1c88a6f5534ae0fa1ed72c28769f92dccb3722e0
Qilton <swayambhalotia@gmail.com>
2024-09-03 20:40:44 +0530

    huh

---
+++ client/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react' 
+import './App.css'
+import { useForm } from "react-hook-form"
+ 
+ 
+
+function App() { 
+  const {
+    register,
+    handleSubmit,
+    setError,    
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  
+
+  const onSubmit = async (data) => {
+    
+    let r = await fetch("http://localhost:3000/submit", {method: "POST",  headers: {
+      "Content-Type": "application/json", 
+    }, body: JSON.stringify(data)})
+    let res = await r.text()
+    console.log(data, res)
+  
+  }
+
+  return (
+    <> 
+  
+       <div className="container">
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <input placeholder='username' {...register("username")}/>
+          <br />
+          <input placeholder='password'  {...register("password")}/>
+         
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+       </div>
+    </>
+  )
+}
+
+export default App
